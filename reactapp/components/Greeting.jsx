fbd7d14caa3d4e2a75a3d6b4201a408af2f7b5b4
Iamneo <techadmin@iamneo.ai>
2023-09-27 07:00:49 +0000

    Backup Commit

--- reactapp/components/Greeting.jsx
+++ reactapp/components/Greeting.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Greeting  =(props) =>
+function Greeting (props)
 {
+    // const[name,SetName] = useState('');
     return(
         <div>
             <h1>Hey!!! Greeting</h1>
             <form className="greet">
                 <label for="name">Enter your name:</label>
                 <input placeholder="User" id="name"></input>
-                <button type="submit">Say Hello</button>
+                <button type="submit" onClick={}>Say Hello</button>
             </form>
             <div> Name: {props.name} </div>
         </div>
