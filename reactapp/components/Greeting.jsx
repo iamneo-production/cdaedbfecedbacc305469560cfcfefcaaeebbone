c21167541a26f327f169c77034d086a5fb672f8a
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:50:44 +0000

    Backup Commit

--- reactapp/components/Greeting.jsx
+++ reactapp/components/Greeting.jsx
@@ -5,10 +5,10 @@ const Greeting  =(props) =>
     return(
         <div>
             <h1>Hey!!! Greeting</h1>
-            <form>
-                <label>
-                    
-                </label>
+            <form className="greet">
+                <label for="name">Enter your name:</label>
+                <input placeholder="User" id="name"></input>
+                <button type="submit">Say Hello</button>
             </form>
             <div> Name: {props.name} </div>
         </div>
